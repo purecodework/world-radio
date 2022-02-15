2057b1c05596e808edf7e60dac9b8d8923105189
purecodework <68151429+purecodework@users.noreply.github.com>
2022-02-15 23:21:34 +1100

    update player style

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -6,15 +6,23 @@ const Player = (props) => {
   return (
     <div className="flex border-none w-full max-w-sm mb-2 ">
       <AudioPlayer
-        className="flex border-none shadow rounded-lg items-center"
+        className=""
         src={props.radio.urlResolved}
         customControlsSection={[
-          <img src={props.radio.favicon} className="object-cover h-10 w-10" />,
-          <p className="overflow-hidden w-3/5 no-scrollbar whitespace-nowrap text-sm">
-            <p>On air now:</p>
-            {props.radio ? props.radio.name : null}
-          </p>,
-          <Star radio={props.radio} />,
+          // <img src={props.radio.favicon} className="object-cover h-10 w-10" />,
+          // <p className="overflow-hidden w-3/5 no-scrollbar whitespace-nowrap text-sm">
+          //   <p>On air now:</p>
+          //   {props.radio ? props.radio.name : null}
+          // </p>,
+          // <Star radio={props.radio} />,
+          <div className="flex w-4/5 items-center">
+            <img src={props.radio.favicon} className="object-cover h-10 w-10" />
+            <p className="overflow-hidden w-3/5 no-scrollbar whitespace-nowrap text-sm mx-5">
+              <p className="text-slate-500">On air now:</p>
+              {props.radio ? props.radio.name : null}
+            </p>
+            <Star radio={props.radio} />
+          </div>,
           RHAP_UI.MAIN_CONTROLS,
         ]}
         customProgressBarSection={[]}
